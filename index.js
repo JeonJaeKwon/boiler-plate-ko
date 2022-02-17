4396cc539c61d2191984737d05bc148feb7ea44e
JeonJaeKwon <jjk2615@gmail.com>
2022-02-17 12:15:06 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+const { User } = require("./models/User");
+
+//application/x-www/form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jaekwon38:inkwnp9z1!@cluster0.cqoqc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.send('Hello World!! 안녕하세요'))
+
+app.post('/register', (req, res) => {
+
+    // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+    // 그것들을 DB에 넣어준다.
+
+    const user = new User(req.body);
+
+    user.save((err, useInfo) => {
+        if(err) return res.json({sucess: false, err})
+        return res.status(200).json({
+            sucess: true
+        })
+    })
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
